8976fd285d74662d8f01e4ecc188303d6fe9e16d
kofiarkoh <kofiarkoh0@gmail.com>
2022-09-20 21:51:26 +0000

    remove default app component

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -12,19 +12,6 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import home.HomeLayout
 
-@Composable
-@Preview
-fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
-
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
-        }
-    }
-}
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
